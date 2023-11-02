2953cfec73d8961d156ae3d6764b57af28cb1047
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2023-11-02 01:32:40 -0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Navbar'
 import Hero from './components/Hero'
-import Services from './pages/services'
-import Porject from './pages/Porject'
 import Project from './pages/Porject'
+import Services from './pages/services'
 import Works from './pages/Works'
 
 function App() {
